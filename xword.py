db0564d62135f347f77a979ddd22abdb967e9b33
bschue <bschuess3@gmail.com>
2018-11-12 09:57:36 -0500

    Returns list of matching words

--- xword.py
+++ xword.py
@@ -1,21 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Bschues"
+
+import re
+
 
 # YOUR HELPER FUNCTION GOES HERE
+def find_matches(test_word, words):
+    test_word = test_word.replace(' ', '\\w')
+    matches = []
+    for word in words:
+        if len(word) == len(test_word):
+            match = re.search(test_word, word)
+            if match:
+                matches.append(match.group())
+            # print('hit')
+    return(matches)
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
-
-    test_word = raw_input(
-        'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
-
+    test_word = str(input(
+        'Enter a word to solve.\nUse spaces for unknown letters: ').lower())
+    print(find_matches(test_word, words))
     # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    # raise NotImplementedError('Please complete this')
 
 
 if __name__ == '__main__':
